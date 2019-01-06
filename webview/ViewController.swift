c7e7b909cedc904a52e7dbe9d5d66cd18096b51b
Admin <admin@Admins-Mac.local>
2019-01-06 00:28:39 -0800

    Initial Commit

---
+++ webview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  webview
+//
+//  Created by Admin on 06/01/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
